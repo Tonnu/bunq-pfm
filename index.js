e1bdaddd65cc257f01b5ec022c5e02623bd06d5d
Toon Verbeek <toon.verbeek1@gmail.com>
2017-08-06 19:12:40 +0200

    added firebase

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Container, Row, Col } from 'react-grid-system';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import * as firebase from 'firebase';
 
 import PFM from './components/views/PFM.js';
 
